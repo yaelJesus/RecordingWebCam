7ceba164a177b77f3e9f4c223a910a7ccd22f773
yaelJesus <77377276+yaelJesus@users.noreply.github.com>
2024-09-04 11:39:31 -0600

    Charged Files

---
+++ RecordingWebCam/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace RecordingWebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnOff = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(320, 12);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "&Grabar";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(713, 12);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "&Capturar";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(302, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RecordingWebCam.Properties.Resources.Patricio;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(383, 397);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(406, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(383, 397);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(401, 12);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(75, 23);
+            this.btnOff.TabIndex = 5;
+            this.btnOff.Text = "&Apagar";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.btnRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnOff;
+    }
+}
+
---
+++ RecordingWebCam/Form1.cs
@@ -0,0 +1,95 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecordingWebCam
+{
+    public partial class Form1 : Form
+    {
+        private string Path = @"C:\Users\abdie\Documents\RecordingWebCam\RecordingWebCam\";
+        private bool DevicesAreFound;
+        private FilterInfoCollection MyDevices;
+        private VideoCaptureDevice MyWebCam;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ChargeDevices();
+        }
+
+        public void ChargeDevices()
+        {
+            MyDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (MyDevices.Count > 0)
+            {
+                DevicesAreFound = true;
+                for (int i = 0; i < MyDevices.Count; i++)
+                {
+                    comboBox1.Items.Add(MyDevices[i].Name.ToString());
+                    comboBox1.Text = MyDevices[0].ToString();
+
+                }
+            }
+            else
+            {
+                DevicesAreFound = false;
+            }
+        }
+
+        public void Recording(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            Imagen.RotateFlip(RotateFlipType.RotateNoneFlipX);
+
+            pictureBox1.Image = Imagen;
+        }
+
+        private void CloseWebCam()
+        {
+            if (MyWebCam != null && MyWebCam.IsRunning)
+            {
+                MyWebCam.SignalToStop();
+                MyWebCam = null;
+            }
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            CloseWebCam();
+            int i = comboBox1.SelectedIndex;
+            string NombreVideo = MyDevices[i].MonikerString;
+            MyWebCam = new VideoCaptureDevice(NombreVideo);
+            MyWebCam.NewFrame += new NewFrameEventHandler(Recording);
+            MyWebCam.Start();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            if (MyWebCam != null && MyWebCam.IsRunning)
+            {
+                pictureBox2.Image = pictureBox1.Image;
+                pictureBox2.Image.Save(Path + "ImageCapture.jpg", ImageFormat.Jpeg);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseWebCam();
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            CloseWebCam();
+            pictureBox1.Image = Image.FromFile(@"C:\Users\abdie\Documents\RecordingWebCam\RecordingWebCam\Resources\Patricio.jpg");
+        }
+    }
+}
